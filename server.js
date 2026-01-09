41263d07d2d6761ed97087bd1bcd89fc22463aa1
Octopicsus <chobotnicecz@gmail.com>
2026-01-10 00:15:32 +0100

    settings refactor 4.1

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ const app = express()
 
 const PORT = require("./config/port.js")
 
-const apiGauge = require("./src/api/gaugeValue")
-const apiCarousel = require("./src/api/carouselValue")
 const apiSettings = require("./src/api/settingsMap")
 const apiValuesMap = require("./src/api/valuesMap.js")
 
@@ -13,8 +11,6 @@ app.use(express.json())
 app.use(cors())
 
 app.use("/api", apiSettings)
-app.use("/api", apiGauge)
-app.use("/api", apiCarousel)
 app.use("/api", apiValuesMap)
 
 app.listen(PORT, () => {
