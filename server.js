69d03b58c99573487c614921a6051264ff4ee5bc
Octopicsus <chobotnicecz@gmail.com>
2026-01-10 15:05:37 +0100

    settings refactor 4.5

--- server.js
+++ server.js
@@ -4,14 +4,12 @@ const app = express()
 
 const PORT = require("./config/port.js")
 
-const apiSettings = require("./src/api/settingsMap")
-const apiValuesMap = require("./src/api/valuesMap.js")
+const apiSettingsMap = require("./src/api/settingsMap.js")
 
 app.use(express.json())
 app.use(cors())
 
-app.use("/api", apiSettings)
-app.use("/api", apiValuesMap)
+app.use("/api", apiSettingsMap)
 
 app.listen(PORT, () => {
   console.log(`Server started: ${PORT}`)
--- src/api/settingsMap.js
+++ src/api/settingsMap.js
@@ -4,6 +4,31 @@ const router = express.Router()
 const { readDb, writeDb } = require("../functions/db")
 const linkDB = "../db/valueDB.json"
 
+const responseData = (item) => {
+  if (item.type === "carousel") {
+    return {
+      id: item.id,
+      type: item.type,
+      title: item.title,
+      value: item.value,
+      defaultValue: item.defaultValue,
+      options: item.options,
+      action: item.action,
+    }
+  }
+
+  if (item.type === "gauge") {
+    return {
+      id: item.id,
+      type: item.type,
+      title: item.title,
+      value: item.value,
+      defaultValue: item.defaultValue,
+      action: item.action,
+    }
+  }
+}
+
 router.get("/settings", async (req, res) => {
   const db = await readDb(linkDB)
   res.json(db)
@@ -11,14 +36,41 @@ router.get("/settings", async (req, res) => {
 
 router.post("/settings/reset", async (req, res) => {
   const db = await readDb(linkDB)
-  
+
   db.forEach((item) => {
     item.value = item.defaultValue
   })
 
   await writeDb(db, linkDB)
 
-  res.json( db )
+  res.json(db)
+})
+
+router.get("/settings/values", async (req, res) => {
+  const db = await readDb(linkDB)
+  const id = Number(req.query.id)
+
+  const item = db.find((widget) => widget.id === id)
+
+  res.json(responseData(item))
+})
+
+router.post("/settings/values", async (req, res) => {
+  const db = await readDb(linkDB)
+  const id = Number(req.body.id)
+  const value = Number(req.body.value)
+
+  const item = db.find((widget) => widget.id === id)
+  item.value = value
+
+  await writeDb(db, linkDB)
+
+  res.json(responseData(item))
+})
+
+router.get("/settings", async (req, res) => {
+  const db = await readDb(linkDB)
+  res.json(db)
 })
 
 module.exports = router
